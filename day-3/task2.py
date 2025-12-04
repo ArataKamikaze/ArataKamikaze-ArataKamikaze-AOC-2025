6734c18841a2fc35dbb496d0d0194c343605cc5f
aratakamikaze <victor12cunha@gmail.com>
2025-12-04 01:06:51 -0300

    day 3 - task 2

--- day-3/task2.py
+++ day-3/task2.py
@@ -1,19 +1,34 @@
 sum = 0
+length = 2
+
+def getMaxNum(string, min, max):
+    maxNum = 0
+    pos = min
+    i = min
+    oldString = string
+    
+    if max == 0:
+        max = None
+    string = string[min:max]
+    for num in string:
+        if(int(num)> maxNum):
+            maxNum = int(num)
+            pos = i
+        i+=1
+    return maxNum, pos+1
+
 try:
     with open('input.txt', 'r') as input:
         for line in input.readlines():
-                lineMax = 0
-                max1num = 0
-                i = 0
-                for num in line.strip():
-                    if(int(num) >= int(max1num)):
-                        max1num = num;
-                        for num2 in range(i+1,len(line)):    
-                            if(int(num+str(line[num2])) > lineMax):
-                                lineMax = int(str(num)+str(line[num2]))
-                    i+=1
-                print(lineMax)
-                sum+=lineMax
+            numStr = ''
+            min = 0
+            max = length-1
+            j = 0
+            for pos in range(length):
+                maxNum, min = getMaxNum(line.strip(), min, max*-1)
+                max = length-1-pos-1
+                numStr+=str(maxNum)
+            sum+= int(numStr)                    
 except FileNotFoundError:
     print("Error: The file was not found.")
 print(sum)
